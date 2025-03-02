23fa5a7881b84c6e0870b2ada96f4a7e1ee3efe5
bhushan alva <bhushanalva1234@gmail.com>
2025-03-02 22:51:30 +0530

    added base html and link pages

--- portfolio/MoneyMap/urls.py
+++ portfolio/MoneyMap/urls.py
@@ -3,6 +3,16 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='money_map_home'),
+    path('', views.home, name='money_map_home'),
+    path('income/', views.income, name='money_map_income'),
+    path('accounts/', views.accounts, name='money_map_accounts'),
+    path('budget/', views.budget, name='money_map_budget'),
+    path('expense/', views.expense, name='money_map_expense'),
+    path('goals/', views.goals, name='money_map_goals'),
+    path('insights/', views.insights, name='money_map_insights'),
+    path('settings/', views.settings, name='money_map_settings'),
+    path('transaction/', views.transaction, name='money_map_transaction'),
+
+    
 ]
 
--- portfolio/MoneyMap/views.py
+++ portfolio/MoneyMap/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
+def home(request):
     return render(request, "MoneyMap/Home.html")
+
+def accounts(request):
+    return render(request, "MoneyMap/accounts.html")
+
+def budget(request):
+    return render(request, "MoneyMap/budget.html")
+
+def expense(request):
+    return render(request, "MoneyMap/expense.html")
+
+def goals(request):
+    return render(request, "MoneyMap/goals.html")
+
+def income(request):
+    return render(request, "MoneyMap/income.html")
+
+def insights(request):
+    return render(request, "MoneyMap/insights.html")
+
+def settings(request):
+    return render(request, "MoneyMap/settings.html")
+
+def transaction(request):
+    return render(request, "MoneyMap/transaction.html")
+
